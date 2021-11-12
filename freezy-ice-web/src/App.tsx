f6c651df4f9153a79242825d66192e2dd465c927
Bartosz Skrabka <56127541+Skrabka98@users.noreply.github.com>
2021-11-12 18:07:22 +0100

    #3 Layout (#18)
    * Layout and eslint fixes
    
    * Fix default margin, prettier formating
    
    * remove unnecessary configs
    
    * Change primary color

--- freezy-ice-web/src/App.tsx
+++ freezy-ice-web/src/App.tsx
@@ -1,10 +1,20 @@
+import * as dotenv from 'dotenv';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import Router from './router';
+import { Store } from './Store';
+
 function App() {
-  return (
-    <div>
-      <header>
-        <p>Freezy-Ice-Page</p>
-      </header>
-    </div>
-  );
+    dotenv.config();
+
+    return (
+        <Provider store={Store}>
+            <BrowserRouter>
+                <Router />
+            </BrowserRouter>
+        </Provider>
+    );
 }
+
 export default App;
--- freezy-ice-web/src/index.tsx
+++ freezy-ice-web/src/index.tsx
@@ -1,12 +1,28 @@
+import { createTheme, ThemeProvider } from '@mui/material';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as dotenv from 'dotenv';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './index.css';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#81E2DC',
+        },
+    },
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
+    <React.StrictMode>
+        <ThemeProvider theme={theme}>
+            <App />
+        </ThemeProvider>
+    </React.StrictMode>,
+    document.getElementById('root'),
 );
+
+dotenv.config();
+
 serviceWorker.unregister();
