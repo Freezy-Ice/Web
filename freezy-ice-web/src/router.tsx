e3dc75957590fbd551620fe26041fe9a71d62387
Ola-M <56175718+Ola-M@users.noreply.github.com>
2021-11-16 22:03:16 +0100

    #5 Login page (#20)
    * Login page
    
    * add props
    
    * fix
    
    * change names

--- freezy-ice-web/src/router.tsx
+++ freezy-ice-web/src/router.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Redirect, Switch } from 'react-router-dom';
 import Home from './Pages/Home';
 import Layout from './Components/Layout/Layout';
+import Login from './Components/Login';
 import { RouterPathEnum } from './Helpers/enums';
 import Error404 from './Pages/Error404';
 
@@ -9,6 +10,7 @@ function Router() {
     return (
         <Switch>
             <Layout exact path={RouterPathEnum.HOME} component={Home} />
+            <Layout exact path={RouterPathEnum.LOGIN} component={Login} />
             <Layout exact={false} path={RouterPathEnum.Error404} component={Error404} />
             <Redirect to={RouterPathEnum.Error404} />
         </Switch>
